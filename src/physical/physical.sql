08ceecd78120e792697473d27287db6a489d220f
songjs1993 <1390880230@qq.com>
2016-05-09 21:00:29 +0800

    Update physical.sql

--- src/physical/physical.sql
+++ src/physical/physical.sql
@@ -6,7 +6,7 @@ create type vertex_ AS(flag int,to_node int,value graphh_value);
 create table vertex(flag int,value graphh_value) distributed by (flag);
 create table edge(from_node int,to_node int) distributed by (from_node);
 
-// 后续版本考虑加入以顶点为中心算法模型中的active设计考虑，需要加入v_update表来模拟顶点活跃
---create table v_update(flag int,value graphh_value) distributed by (flag);   
+-- 后续版本考虑加入以顶点为中心算法模型中的active设计考虑，需要加入v_update表来模拟顶点活跃
+-- create table v_update(flag int,value graphh_value) distributed by (flag);   
 
 
